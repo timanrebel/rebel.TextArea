bc27263ee5a9afad3b9d180ef5290744570ab7d7
Timan Rebel <timan@rebelic.nl>
2014-05-25 13:25:33 +0200

    Initial commit

---
+++ controllers/widget.js
@@ -0,0 +1,56 @@
+_.extend(this, {
+	value: null,
+
+	construct: function(config) {
+		$.textarea.applyProperties(_.omit(config, 'id', '__parentSymbol', '__itemTemplate', '$model'));
+
+		$.value = '';
+
+		if (OS_IOS)
+			$.label.applyproperties({
+				text: config.hintText,
+				font: config.font
+			});
+	},
+
+	focus: function() {
+		$.textarea.focus();
+	},
+
+	blur: function() {
+		$.textarea.blur();
+	},
+
+	addEventListener: function(type, callback) {
+		$.on(type, callback);
+	},
+
+	removeEventListener: function(type, callback) {
+		$.off(type, callback);
+	},
+
+	destruct: function() {
+		$.off();
+	}
+});
+
+function onFocus(evt) {
+	if (OS_IOS) {
+		$.label.hide();
+	}
+
+	$.trigger(evt.type, evt);
+}
+
+function onBlur(evt) {
+	if (OS_IOS) {
+		if (!this.hasText())
+			$.label.show();
+	}
+
+	$.trigger(evt.type, evt);
+}
+
+function onChange(evt) {
+	$.value = this.value;
+}
